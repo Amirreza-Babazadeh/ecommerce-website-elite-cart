7efb06f2f590b7a58dd4cfa1a9a6f322038e23b1
Amirreza-Babazadeh <amirrezabbss@gmail.com>
2025-11-19 23:56:40 +0330

    return page is finished

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -1,1 +1,31 @@
-/*=============== ACCORDION ===============*/
+/* ACCORDION */
+const accordionItems = document.querySelectorAll(".accordion-item")
+
+accordionItems.forEach((item) => {
+  const header = item.querySelector(".accordion-header"),
+    content = item.querySelector(".accordion-content"),
+    icon = header.querySelector("i")
+
+  header.addEventListener("click", () => {
+    const isOpen = item.classList.toggle("accordion-open")
+
+    content.style.height = isOpen ? content.scrollHeight + "px" : "0px"
+    icon.className = isOpen ? "ri-subtract-line" : "ri-add-line"
+
+    // remove other accordion
+    accordionItems.forEach((otherItem) => {
+      if (
+        otherItem !== item &&
+        otherItem.classList.contains("accordion-open")
+      ) {
+        otherItem.classList.remove("accordion-open")
+
+        const otherContent = otherItem.querySelector(".accordion-content")
+        const otherIcon = otherItem.querySelector(".accordion-header i")
+
+        otherContent.style.height = "0px"
+        otherIcon.className = "ri-add-line"
+      }
+    })
+  })
+})
