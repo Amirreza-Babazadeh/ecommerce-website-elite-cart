f35e4ddc071cc2ba06d1f43bfcb33c8555dbbddf
Amirreza-Babazadeh <amirrezabbss@gmail.com>
2025-11-27 22:42:02 +0330

    larg screen responsive is finished

--- assets/js/home.js
+++ assets/js/home.js
@@ -3,22 +3,37 @@ var homeSwiper = new Swiper(".home-swiper", {
   loop: true,
   pagination: {
     el: ".swiper-pagination",
-    clickable: true,
   },
 })
 
 /*=============== PRODUCT SWIPER ===============*/
-var homeSwiper = new Swiper(".product-swiper", {
+var productSwiper = new Swiper(".product-swiper", {
   loop: true,
-  slidesPerView: 3,
-  spaceBetween: 30,
+
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    1400: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
 })
 
 /*=============== ELEVATE SWIPER ===============*/
-var homeSwiper = new Swiper(".product-elevate-swiper", {
+var elevateSwiper = new Swiper(".product-elevate-swiper", {
   loop: true,
   pagination: {
     el: ".swiper-pagination",
-    clickable: true,
   },
 })
